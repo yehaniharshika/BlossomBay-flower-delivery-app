f7de4b33cc62de7c2653407d7e90b72ad37c9c94
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-16 16:06:29 +0530

    changed ProductItem

--- flower-delivery-frontend/src/comoponents/ProductItem/ProductItem.jsx
+++ flower-delivery-frontend/src/comoponents/ProductItem/ProductItem.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductItem.css'
 import { assets } from '../../assets/assets'
 
 const ProductItem = ({ id, name, price, description, image}) => {
+  const [itemCount,setItemCount] = useState(0);
   return (
     <div className='product-item'>
       <div className="product-item-img-container">
         <img className='product-item-image' src={image} alt=''/>
+        {!itemCount
+          ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=''/>
+          :<div className='product-item-counter'>
+              <img onClick={() => setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''/>
+              <p>{itemCount}</p>
+              <img onClick={() => setItemCount(prev=prev+1)} src={assets.add_icon_green} alt=''/>
+          </div>
+        }
       </div>
 
       <div className="product-item-info">
