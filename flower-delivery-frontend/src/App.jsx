6a8d383e592045c6f7bbf2991e0d6a45b973aa4d
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-16 10:13:00 +0530

    first commit

---
+++ flower-delivery-frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Navigationbar from './comoponents/Navigationbar/Navigationbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navigationbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
