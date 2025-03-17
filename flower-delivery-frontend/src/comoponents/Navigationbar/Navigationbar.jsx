7404714acfc01f0d91cfa782f91d910116665aca
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-17 09:46:25 +0530

    created basic styles for Cart.jsx

--- flower-delivery-frontend/src/comoponents/Navigationbar/Navigationbar.jsx
+++ flower-delivery-frontend/src/comoponents/Navigationbar/Navigationbar.jsx
@@ -8,7 +8,7 @@ const Navigationbar = ({setShowLogin}) => {
 
   return (
     <div className="navbar">
-      <h1 className="logo">BlossomBay</h1>
+      <Link to='/'><h1 className="logo">BlossomBay</h1></Link>
       <ul className="navbar-menu">
         <Link onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
         <a href="#explore-products" onClick={() => setMenu("products")} className={menu === "products" ? "active" : ""}>Product</a>
@@ -19,7 +19,7 @@ const Navigationbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className="dot"></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
--- flower-delivery-frontend/src/pages/Cart/Cart.jsx
+++ flower-delivery-frontend/src/pages/Cart/Cart.jsx
@@ -19,6 +19,23 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map((item,index)=> {
+          if(cartItems[item._id]>0){
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt=''/>
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price*cartItems[item._id]}</p>
+                <p className='cross'>x</p>
+              </div>
+              <hr />
+              </div>
+            )
+          }
+        })}
       </div>  
     </div>
   )
