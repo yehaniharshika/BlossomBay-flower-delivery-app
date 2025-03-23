750d9a697370298fd4194f9c6a2f32689fc323a4
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 13:15:53 +0530

    created styles for List.jsx

--- admin-part/src/pages/List/List.jsx
+++ admin-part/src/pages/List/List.jsx
@@ -25,9 +25,29 @@ const List = () => {
         fetchList();
     },[])
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+    <p className='list-title'>All Flowers List</p>
+    <div className="list-table">
+      <div className="list-table-format title">
+        <b>Image</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b>Action</b>
+      </div>
+      {list.map((item,index)=>{
+        return(
+          <div key={index} className='list-table-format'>
+            <img src={`${url}/images/`+ item.image} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>Rs: {item.price}</p>
+            <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
+          </div>
+        )
+      })}
     </div>
+  </div>
   )
 }
 
