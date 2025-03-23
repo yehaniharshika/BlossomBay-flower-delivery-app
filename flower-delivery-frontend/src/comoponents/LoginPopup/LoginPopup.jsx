62bf18fa08a678ba157013552180874b703f6442
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 18:10:48 +0530

    connect login process to frontend

--- flower-delivery-frontend/src/comoponents/LoginPopup/LoginPopup.jsx
+++ flower-delivery-frontend/src/comoponents/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LoginPopup.css";
 import { assets } from "../../assets/assets";
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [currState, setCurrState] = useState("Sign Up");
+  const [currState, setCurrState] = useState("Login");
+  const [data,setData] = useState({
+    name : "",
+    email : "",
+    password : ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  // useEffect(() => {
+  //   console.log(data); 
+  // },[data]);
 
   return (
     <div className="login-popup">
@@ -13,9 +28,9 @@ const LoginPopup = ({ setShowLogin }) => {
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
-              {currState==="Login"?<></>:<input type="text" placeholder='Your name' required/>}
-              <input type="email" placeholder='Your email' required />
-              <input type="password" placeholder='Password' required />
+              {currState==="Login"?<></>:<input name="name" onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required/>}
+              <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
+              <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
             </div>
             <button type='submit'>{currState==="Sign Up"?"Create account":"Login"}</button>
 
