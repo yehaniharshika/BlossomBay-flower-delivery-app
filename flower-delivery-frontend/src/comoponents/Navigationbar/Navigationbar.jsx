f2ad75023c33be22915970341c6802711c22fb8c
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 21:49:52 +0530

    changed NavBar.css

--- flower-delivery-frontend/src/comoponents/Navigationbar/Navigationbar.jsx
+++ flower-delivery-frontend/src/comoponents/Navigationbar/Navigationbar.jsx
@@ -8,7 +8,7 @@ import { StoreContext } from "../../context/StoreContext";
 const NavigationBar = ({setShowLogin}) => {
 
     const[menu,setMenu] = useState("home");
-    const {getTotalCartAmount} = useContext(StoreContext);
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
   return (
     <div className="navbar">
@@ -27,7 +27,17 @@ const NavigationBar = ({setShowLogin}) => {
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={() => setShowLogin(true)}>sign in</button>
+        {!token?<button onClick={() => setShowLogin(true)}>sign in</button>
+        :<div className="navbar-profile">
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr/>
+              <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>
+          
+          </div>}
+        
       </div>
     </div>
   );
