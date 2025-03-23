93ee0be9f86af06906e389edb6821f42a2c26ebe
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 12:56:03 +0530

    success flower add process

--- admin-part/src/pages/Add/Add.jsx
+++ admin-part/src/pages/Add/Add.jsx
@@ -1,43 +1,90 @@
 import React, { useEffect, useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Add = () => {
-    const [image,setImage] =  useState(false);
-    const [data,setData] = useState({
+  const url = "http://localhost:4000";
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Lily",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/flower/add`, formData);
+    if (response.data.success) {
+      setData({
         name: "",
         description: "",
         price: "",
-        category:"Lily"
-    })
-
-    const onChangeHandler = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setData(data => ({...data,[name]:value})); 
+        category: "Lily",
+      });
+      setImage(false);
+      toast.success(response.data.message, {
+        style: { fontFamily: "Montserrat, sans-serif",fontWeight:"700" },
+      });
+    } else {
+      toast.error(response.data.message, {
+        style: { fontFamily: "Montserrat, sans-serif",fontWeight:"700" },
+      });
     }
-
-
+  };
 
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-image-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </div>
 
         <div className="add-product-name flex-col">
           <p>Product name</p>
-          <input onChange={onChangeHandler} value={data.name} type="text" name="name" placeholder="Type here" />
+          <input
+            onChange={onChangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
         </div>
 
         <div className="add-product-description flex-col">
           <p>Product description</p>
-          <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6"
+          <textarea
+            onChange={onChangeHandler}
+            value={data.description}
+            name="description"
+            rows="6"
             placeholder="Write content here"
             required
           ></textarea>
@@ -61,10 +108,18 @@ const Add = () => {
 
           <div className="add-price flex-col">
             <p>Product price</p>
-            <input onChange={onChangeHandler} value={data.price} type="Number" name="price" placeholder="Rs : " />
+            <input
+              onChange={onChangeHandler}
+              value={data.price}
+              type="Number"
+              name="price"
+              placeholder="Rs : "
+            />
           </div>
         </div>
-        <button type='submit' className='add-btn'>ADD</button>
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
       </form>
     </div>
   );
