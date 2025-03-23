0f8475a2966e699966ec214e6ac3e4b44ec632c6
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 11:07:44 +0530

    created Navbar.jsx and applied styles

--- admin-part/src/components/Navbar/Navbar.jsx
+++ admin-part/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./Navbar.css"
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
+        <h1 className="logo">BlossomBay</h1>
+        <img className="profile" src={assets.profile_image} alt=''/>
+        
+
       
     </div>
   )
