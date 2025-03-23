61f745003f79ac7eda31956e0ec56cbde2ecd03f
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-23 13:09:19 +0530

    created basic structure List.jsx file

--- admin-part/src/pages/List/List.jsx
+++ admin-part/src/pages/List/List.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios  from 'axios';
+import { toast } from 'react-toastify';
 
 const List = () => {
+
+    const url = "http://localhost:4000"
+    const [list,setList] = useState([]);
+
+    const fetchList = async () => {
+        const response = await axios.get(`${url}/api/flower/list`);
+        console.log(response.data);
+        
+        if(response.data.success){
+            setList(response.data.data);
+        }else {
+            toast.error("Error", {
+                style: { fontFamily: "Montserrat, sans-serif",fontWeight:"700" },
+            })
+        }
+    }
+
+    useEffect(() => {
+        fetchList();
+    },[])
   return (
     <div>
       
