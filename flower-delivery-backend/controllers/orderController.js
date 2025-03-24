8944206979a3d3b398062d3b4714f2a040c7629e
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-24 18:01:58 +0530

    created orderController.js

---
+++ flower-delivery-backend/controllers/orderController.js
@@ -0,0 +1,8 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+// placing user order for frontend
+const placeOrder = async(req,res) => {
+    
+}
