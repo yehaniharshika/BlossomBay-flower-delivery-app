86e27446c642c7c7c05c939c4e9d368aca99dae9
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-24 13:32:44 +0530

    created cartRotes.js

---
+++ flower-delivery-backend/routes/cartRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { addToCart,removeFromCart,getCart } from '../controllers/cartController.js'
+
+const cartRouter = express.Router();
+
+cartRouter.post("/add",addToCart);
+cartRouter.post("/remove",removeFromCart);
+cartRouter.get("/get",getCart);
