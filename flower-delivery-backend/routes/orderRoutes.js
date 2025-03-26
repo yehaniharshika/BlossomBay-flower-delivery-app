0b84e793db5f9dd93865276350b0c8b3994472d7
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-26 08:57:22 +0530

    created get all user orders function

--- flower-delivery-backend/routes/orderRoutes.js
+++ flower-delivery-backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/placeOrder",authMiddleware,placeOrder);
 orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/userOrders",authMiddleware,userOrders);
 
 export default orderRouter;
