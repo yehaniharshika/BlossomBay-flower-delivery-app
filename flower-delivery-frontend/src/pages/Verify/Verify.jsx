0e3340ff4fb6795c1d817e26403f3315dd21cfda
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-26 10:59:21 +0530

    created verify order in frontend

--- flower-delivery-frontend/src/pages/Verify/Verify.jsx
+++ flower-delivery-frontend/src/pages/Verify/Verify.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import "./Verify.css"
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const Verify = () => {
 
@@ -10,6 +11,20 @@ const Verify = () => {
   const orderId = searchParams.get("orderId");
   const { url } = useContext(StoreContext);
   const navigate = useNavigate();
+
+  const verifyPayment = async() => {
+    const response = await axios.post(url + "/api/order/verify", {success,orderId});
+
+    if (response.data.success) {
+      navigate("/myOrders");
+    } else {
+      navigate("/");
+    }
+  }
+
+  useEffect(() => {
+    verifyPayment();
+  },[])
   
   return (
     <div className='verify'>
