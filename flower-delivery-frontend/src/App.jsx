0b1aa56c696abd780c0eacf71e27f3770dc21205
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-26 12:29:26 +0530

    created load my orders in frontend

--- flower-delivery-frontend/src/App.jsx
+++ flower-delivery-frontend/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
-          <Route path="/myOrder" element={<MyOrders />} />
+          <Route path="/myOrders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer/>
