bcdabc66030a5ccdb418e5ccb956363fcb89918d
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-26 10:18:14 +0530

    created Verify.jsx

--- flower-delivery-frontend/src/pages/Verify/Verify.jsx
+++ flower-delivery-frontend/src/pages/Verify/Verify.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Verify.css"
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Verify = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
+  
   return (
-    <div>
+    <div className='verify'>
+      <div className="spinner"></div>
       
     </div>
   )
