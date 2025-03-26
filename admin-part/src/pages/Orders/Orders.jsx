3fdabfe2dd7a20c2acecda265c0fdc2cc403f3d9
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-26 13:14:11 +0530

    implemented fetchAllOrders function

--- admin-part/src/pages/Orders/Orders.jsx
+++ admin-part/src/pages/Orders/Orders.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Orders.css'
+import React from "react";
+import "./Orders.css";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import axios from "axios";
 
-const Orders = () => {
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+  
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error", {
+        style: { fontFamily: "Montserrat, sans-serif", fontWeight: "500" },
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  },[])
+
   return (
     <div>
-      
-    </div>
-  )
-}
 
-export default Orders
+    </div>
+  );
+};
+
+export default Orders;
--- flower-delivery-backend/routes/orderRoutes.js
+++ flower-delivery-backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/placeOrder",authMiddleware,placeOrder);
 orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userOrders",authMiddleware,userOrders);
+orderRouter.get("/list",listOrders);
 
 export default orderRouter;
